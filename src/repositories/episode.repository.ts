936ff1360a5c07f3d796fe11bf0b1d4f40a1c374
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:56:39 +0200

    add 'EpisodeRepository'

---
+++ src/repositories/episode.repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Episode } from '../entities/episode.entity';
+import { DbConnection } from '../enums/dbConnection.enum';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class EpisodeRepository {
+  constructor(
+    @InjectModel(Episode.name, DbConnection.EPISODES)
+    private readonly episodeModel: Model<Episode>,
+  ) {}
+
+  findAll() {
+    return this.episodeModel.find<Episode>();
+  }
+
+  create(body) {
+    return this.episodeModel.create(body);
+  }
+}

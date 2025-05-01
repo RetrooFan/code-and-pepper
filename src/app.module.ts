672fdd6ed9a81ef99584ab6c5ec6887c266b23b5
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:56:34 +0200

    enable 'EpisodesModule'

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { HelloWorldModule } from './modules/helloWorld/helloWorld.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
+import { EpisodesModule } from './modules/episodes/episodes.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] }), HelloWorldModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] }), HelloWorldModule, EpisodesModule],
 })
 export class AppModule {}

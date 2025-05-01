d9c419cda33f445f6c2af2d8b2ed5714dc4a807a
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:56:45 +0200

    implement 'EpisodesController'

--- src/modules/episodes/episodes.controller.ts
+++ src/modules/episodes/episodes.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { EpisodesService } from './episodes.service';
 
-@Controller()
-export class EpisodesController {}
+@Controller('episodes')
+export class EpisodesController {
+  constructor(private readonly episodesService: EpisodesService) {}
+
+  @Get()
+  private findAll() {
+    return this.episodesService.findAll();
+  }
+
+  @Post()
+  private create(@Body() body) {
+    return this.episodesService.create(body);
+  }
+}

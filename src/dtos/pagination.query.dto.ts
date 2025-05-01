53475ff89114377e312b8bc6b1f936be430c1862
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:57:01 +0200

    add 'PaginationQueryDto' class

---
+++ src/dtos/pagination.query.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumber, Max, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class PaginationQueryDto {
+  @Min(0)
+  @IsNumber()
+  @Transform(({ value }) => parseInt(value as string))
+  offset = 0;
+
+  @Min(1)
+  @Max(1000)
+  @IsNumber()
+  @Transform(({ value }) => parseInt(value as string))
+  limit = 10;
+}

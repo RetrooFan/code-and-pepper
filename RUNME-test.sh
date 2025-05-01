3aec267162ed7b0688c4723b1fc99c5f5aeea427
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:56:21 +0200

    update 'RUNME*.sh'

--- RUNME-test.sh
+++ RUNME-test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-docker-compose --file docker-compose.test.yml up --renew-anon-volumes --abort-on-container-exit
+docker-compose -p code-and-pepper-test --file docker-compose.test.yml up --renew-anon-volumes -d
+docker attach code-and-pepper-node-test
+docker stop code-and-pepper-mongo-test
 
 # docker-compose --file docker-compose.test.yml down

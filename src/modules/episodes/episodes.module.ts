3b46c333ad76bc03efdc83227cc46ed094328c3b
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:56:36 +0200

    add Episodes mongo connection

--- src/modules/episodes/episodes.module.ts
+++ src/modules/episodes/episodes.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { EpisodesController } from './episodes.controller';
 import { EpisodesService } from './episodes.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { DbConnection } from '../../enums/dbConnection.enum';
+import { EpisodeEntity, episodeSchema } from '../../entities/episode.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('mongoDbUri'),
+      }),
+      connectionName: DbConnection.EPISODES,
+    }),
+    MongooseModule.forFeature(
+      [
+        {
+          name: EpisodeEntity.name,
+          schema: episodeSchema,
+        },
+      ],
+      DbConnection.EPISODES,
+    ),
+  ],
   controllers: [EpisodesController],
   providers: [EpisodesService],
 })

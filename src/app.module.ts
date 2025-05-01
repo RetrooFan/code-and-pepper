ad2d1d3a27f4e0534b731d3e56823288798a6ebf
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:56:17 +0200

    import 'HelloModule'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { HelloWorldModule } from './modules/helloWorld/helloWorld.module';
 
 @Module({
-  imports: [],
+  imports: [HelloWorldModule],
   controllers: [],
   providers: [],
 })

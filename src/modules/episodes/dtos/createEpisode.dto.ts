ee6b37d93f0f7bc79f816fad974b13f2a7040409
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:57:24 +0200

    add length validators

--- src/modules/episodes/dtos/createEpisode.dto.ts
+++ src/modules/episodes/dtos/createEpisode.dto.ts
@@ -1,6 +1,8 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateEpisodeDto {
   @IsString()
+  @MinLength(1)
+  @MaxLength(32)
   name: string;
 }

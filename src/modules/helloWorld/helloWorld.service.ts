2f1b4f2f260f36ce0aaaa60a8604baa250f0f0b3
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 01:56:27 +0200

    update 'HelloWorldService'

--- src/modules/helloWorld/helloWorld.service.ts
+++ src/modules/helloWorld/helloWorld.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class HelloWorldService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
+    console.log(this.configService.get<string>('nodeEnv'));
+    console.log(this.configService.get<string>('mongoDbUri'));
+
     return 'Hello World!';
   }
 }

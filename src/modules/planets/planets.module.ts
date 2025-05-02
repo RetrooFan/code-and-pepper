91a328660477925cb1d5ae5a278186921277f79b
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 17:04:52 +0200

    add 'PlanetsRepositoryModule'

--- src/modules/planets/planets.module.ts
+++ src/modules/planets/planets.module.ts
@@ -1,37 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigService } from '@nestjs/config';
 
 import { PlanetsController } from './planets.controller';
 import { PlanetsService } from './planets.service';
-import { Planet, planetSchema } from '../../entities/planet.entity';
-import { PlanetsRepository } from './planets.repository';
-import { Character, characterSchema } from '../../entities/character.entity';
+import { PlanetsRepositoryModule } from '../../repositories/planets/planetsRepository.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('mongoDbUri'),
-      }),
-      connectionName: PlanetsRepository.name,
-    }),
-    MongooseModule.forFeature(
-      [
-        {
-          name: Planet.name,
-          schema: planetSchema,
-        },
-        {
-          name: Character.name,
-          schema: characterSchema,
-        },
-      ],
-      PlanetsRepository.name,
-    ),
-  ],
+  imports: [PlanetsRepositoryModule],
   controllers: [PlanetsController],
-  providers: [PlanetsService, PlanetsRepository],
+  providers: [PlanetsService],
 })
 export class PlanetsModule {}
---
+++ src/repositories/planets/planetsRepository.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
+import { Planet, planetSchema } from '../../entities/planet.entity';
+import { PlanetsRepository } from './planets.repository';
+import { Character, characterSchema } from '../../entities/character.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('mongoDbUri'),
+      }),
+      connectionName: PlanetsRepository.name,
+    }),
+    MongooseModule.forFeature(
+      [
+        {
+          name: Planet.name,
+          schema: planetSchema,
+        },
+        {
+          name: Character.name,
+          schema: characterSchema,
+        },
+      ],
+      PlanetsRepository.name,
+    ),
+  ],
+  providers: [PlanetsRepository],
+  exports: [PlanetsRepository],
+})
+export class PlanetsRepositoryModule {}

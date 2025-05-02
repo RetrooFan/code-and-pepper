8c25116c70efafcb493168127bc5e184e1607a16
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-03 00:53:52 +0200

    update 'RUNME*.sh' files

--- RUNME-dev.sh
+++ RUNME-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-docker-compose -p code-and-pepper-dev --file docker-compose.dev.yml up --renew-anon-volumes -d
-docker attach code-and-pepper-dev-node
+docker-compose -p code-and-pepper-dev --file docker-compose.dev.yml up --renew-anon-volumes --no-attach code-and-pepper-dev-mongo --no-log-prefix
 
 # docker stop code-and-pepper-dev-node code-and-pepper-dev-mongo
 # docker-compose down
--- RUNME-test.sh
+++ RUNME-test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-docker-compose -p code-and-pepper-test --file docker-compose.test.yml up --renew-anon-volumes -d
-docker attach code-and-pepper-test-node
-docker stop code-and-pepper-test-mongo
+docker-compose -p code-and-pepper-test --file docker-compose.test.yml up --renew-anon-volumes --no-attach code-and-pepper-test-mongo --no-log-prefix --abort-on-container-exit
 
 # docker-compose --file docker-compose.test.yml down

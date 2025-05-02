34a50fca3594437b4e2ebe9c6f4ef75f819a987f
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-02 17:08:13 +0200

    add 'CharactersRepositoryModule'

--- src/modules/characters/characters.module.ts
+++ src/modules/characters/characters.module.ts
@@ -1,42 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigService } from '@nestjs/config';
 
 import { CharactersController } from './characters.controller';
 import { CharactersService } from './characters.service';
-import { Character, characterSchema } from '../../entities/character.entity';
-import { CharactersRepository } from './characters.repository';
-import { Episode, episodeSchema } from '../../entities/episode.entity';
-import { Planet, planetSchema } from '../../entities/planet.entity';
+import { CharactersRepositoryModule } from '../../repositories/characters/charactersRepository.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('mongoDbUri'),
-      }),
-      connectionName: CharactersRepository.name,
-    }),
-    MongooseModule.forFeature(
-      [
-        {
-          name: Character.name,
-          schema: characterSchema,
-        },
-        {
-          name: Episode.name,
-          schema: episodeSchema,
-        },
-        {
-          name: Planet.name,
-          schema: planetSchema,
-        },
-      ],
-      CharactersRepository.name,
-    ),
-  ],
+  imports: [CharactersRepositoryModule],
   controllers: [CharactersController],
-  providers: [CharactersService, CharactersRepository],
+  providers: [CharactersService],
 })
 export class CharactersModule {}
---
+++ src/repositories/characters/charactersRepository.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
+import { Character, characterSchema } from '../../entities/character.entity';
+import { CharactersRepository } from './characters.repository';
+import { Episode, episodeSchema } from '../../entities/episode.entity';
+import { Planet, planetSchema } from '../../entities/planet.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('mongoDbUri'),
+      }),
+      connectionName: CharactersRepository.name,
+    }),
+    MongooseModule.forFeature(
+      [
+        {
+          name: Character.name,
+          schema: characterSchema,
+        },
+        {
+          name: Episode.name,
+          schema: episodeSchema,
+        },
+        {
+          name: Planet.name,
+          schema: planetSchema,
+        },
+      ],
+      CharactersRepository.name,
+    ),
+  ],
+  providers: [CharactersRepository],
+  exports: [CharactersRepository],
+})
+export class CharactersRepositoryModule {}

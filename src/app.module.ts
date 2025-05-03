48236fea2433decd7d75797a2669382ab86a4ae9
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-03 15:57:32 +0200

    rename 'configuration.ts'

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { HelloWorldModule } from './modules/helloWorld/helloWorld.module';
-import configuration from './configuration';
+import config from './config';
 import { EpisodesModule } from './modules/episodes/episodes.module';
 import { PlanetsModule } from './modules/planets/planets.module';
 import { CharactersModule } from './modules/characters/characters.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     HelloWorldModule,
     EpisodesModule,
     PlanetsModule,

4efba8ab282ff240928ee1722b5ce1a8bb0ae829
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-04 15:29:07 +0200

    add 'ApiProperty' decorators

--- src/dtos/saveEpisode.dto.ts
+++ src/dtos/saveEpisode.dto.ts
@@ -1,8 +1,10 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 import { SaveDto } from './save.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SaveEpisodeDto extends SaveDto {
+  @ApiProperty()
   @IsString()
   @MinLength(1)
   @MaxLength(32)

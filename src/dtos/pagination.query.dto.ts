abbb6b8bc14743ff3ecb90ed00d4957a6d9edc4a
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-04 16:24:14 +0200

    add 'ApiPropertyOptional' decorators

--- src/dtos/pagination.query.dto.ts
+++ src/dtos/pagination.query.dto.ts
@@ -1,18 +1,22 @@
 import { IsIn, IsNumber, IsOptional, Max, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class PaginationQueryDto {
+  @ApiPropertyOptional({ type: Number })
   @Min(0)
   @IsNumber()
   @Transform(({ value }) => parseInt(value as string))
   offset = 0;
 
+  @ApiPropertyOptional({ type: Number })
   @Min(1)
   @Max(1000)
   @IsNumber()
   @Transform(({ value }) => parseInt(value as string))
   limit = 10;
 
+  @ApiPropertyOptional({ type: Number })
   @IsOptional()
   @IsIn([0, 1])
   @Transform(({ value }) => parseInt(value as string))

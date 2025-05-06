909df7fb125dcae9f6ca263218b6027e5e7f5591
Krzysiek Sobol <krzysieksobol93@gmail.com>
2025-05-06 03:07:17 +0200

    update 'RUNME-dev.sh'

--- RUNME-dev.sh
+++ RUNME-dev.sh
@@ -2,5 +2,4 @@
 
 docker-compose -p code-and-pepper-dev --file docker-compose.dev.yml up --renew-anon-volumes --no-attach code-and-pepper-dev-mongo --no-log-prefix
 
-# docker stop code-and-pepper-dev-node code-and-pepper-dev-mongo
-# docker-compose down
+# docker-compose --file docker-compose.dev.yml down
